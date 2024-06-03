de16d73a2aa5c459c66f24062c81a8c2ddbaa759
goodperiodt <ilypsj@gmail.com>
2024-06-03 11:58:29 +0900

    # 1. React-Native 초기 설정 및 개념 확인

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View>
+        {/* 사용자로부터 내용을 입력받으려면 */}
+        <TextInput placeholder='할 일을 입력하세요!' />
+        <Button title='할 일 추가하기' />
+      </View>
+      <View>
+        {/* react native는 style방식을 지정하는 css를 지원하지 않는다. */}
+        <Text style={{ margin: 16 }}>할 일 목록들...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  appContainer: { padding: 50 },
 });
