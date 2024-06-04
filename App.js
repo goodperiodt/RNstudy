2edf617fab00205d105aaa7878a33d48c31b62fe
goodperiodt <ilypsj@gmail.com>
2024-06-04 11:14:43 +0900

    #6. 입력창 모달 구현, 모달 열고 닫기 이벤트 처리

--- App.js
+++ App.js
@@ -12,6 +12,8 @@ import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
+  // 모달의 렌더링 상태를 나타내는 변수
+  const [modalIsVisible, setmodalIsVisible] = useState(false);
   const [todoGoals, setTodoGoals] = useState([]);
 
   // 추가 버튼을 누르면 할 일 목록을 추가하는 함수
@@ -22,6 +24,9 @@ export default function App() {
       ...currentTodoGoals,
       { text: enteredGoalText, id: Math.random().toString() },
     ]);
+
+    // 할 일 추가후 모달이 닫히는
+    endAddGoalHandler();
   };
 
   const deleteGoalHandler = (id) => {
@@ -29,9 +34,32 @@ export default function App() {
       return currentTodoGoals.filter((goal) => goal.id !== id);
     });
   };
+
+  // 할 일 추가 모달을 띄워주는 함수
+  const startAddGoalHandler = () => {
+    setmodalIsVisible(true);
+  };
+
+  // 모달을 닫아주는 함수
+  const endAddGoalHandler = () => {
+    setmodalIsVisible(false);
+  };
+
   return (
     <View style={styles.appContainer}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button
+        title='할 일 추가'
+        color='#5e0acc'
+        onPress={startAddGoalHandler}
+      />
+      {/* modalIsVisible의 상태 값이 true면 GoalInput 컴포넌트를 렌더링한다. */}
+      {/* {modalIsVisible && ( */}
+      <GoalInput
+        visible={modalIsVisible}
+        onAddGoal={addGoalHandler}
+        onCancel={endAddGoalHandler}
+      />
+      {/* )} */}
       <View style={styles.goalsContainer}>
         {/* 
           ScrollView는 전체 화면이 렌더링될 때, 안에 항목들을 전부 렌더링한다.
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { Button, Modal, StyleSheet, TextInput, View } from 'react-native';
 
-const GoalInput = ({ onAddGoal }) => {
+const GoalInput = ({ onAddGoal, onCancel, visible }) => {
   // 입력창의 상태를 관리하는 변수를 React에서 사용하는 useState 훅을 활용하여 선언.
   const [enteredGoalText, setEnteredGoalText] = useState('');
 
@@ -16,15 +16,31 @@ const GoalInput = ({ onAddGoal }) => {
   };
 
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        style={styles.textInput}
-        placeholder='할 일을 입력하세요!'
-        onChangeText={goalInputHandler}
-        value={enteredGoalText}
-      />
-      <Button title='할 일 추가하기' onPress={addGoalHandler} />
-    </View>
+    // Modal 컴포넌트의 속성 visible의 값이 false면 숨겨질 것이고,
+    // true면 나타날 것이다. 굳이 조건부 렌더링을 사용할 필요없이
+    // 속성을 사용한다.
+    <Modal visible={visible} animationType='slide'>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder='할 일을 입력하세요!'
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button
+              title='할 일 추가하기'
+              onPress={addGoalHandler}
+              color='#b180f0'
+            />
+          </View>
+          <View style={styles.button}>
+            <Button title='취소' color='#f31282' onPress={onCancel} />
+          </View>
+        </View>
+      </View>
+    </Modal>
   );
 };
 
@@ -33,18 +49,28 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
+    padding: 16,
+    backgroundColor: '#311b6b',
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8,
+    borderColor: '#e4d0ff',
+    backgroundColor: '#e4d0ff',
+    color: '#120438',
+    borderRadius: 6,
+    width: '100%',
+    padding: 16,
+  },
+
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: 'row',
+  },
+
+  button: {
+    width: '30%',
+    marginHorizontal: 8,
   },
 });
